ea7de0b3b70afdd55527eb3d3ff291924b053c32
Naitoshedo <hozuki@naitoshedo.tv>
2023-10-15 19:19:45 -0500

    Update nerdytec_CYKEY_QMK_ANSI_Keyboard.js

--- nerdytec/nerdytec_CYKEY_QMK_ANSI_Keyboard.js
+++ nerdytec/nerdytec_CYKEY_QMK_ANSI_Keyboard.js
@@ -30,7 +30,7 @@ const vKeys = [
 	14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,               28, //15
 	29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,               43, //15
 	44,	45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 58,                   59, //14
-	60, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,                       73,//13
+	60, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,                       73, //13
 	74,	75,	76,             78,             81, 82, 83,           84, 85, 86  //10
 ];
 
@@ -369,7 +369,7 @@ export function Validate(endpoint)
 	return endpoint.interface === 1;
 }
 
-export function Image()
+export function ImageUrl()
 {
-	return "";
+	return "https://raw.githubusercontent.com/SRGBmods/qmk-plugins/main/_images/nerdytec_CYKEY_QMK_ANSI_Keyboard.png";
 }
