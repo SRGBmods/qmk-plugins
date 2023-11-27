d9c65342c08c25798be0ef25b6c5269d6c8a3445
Naitoshedo <hozuki@naitoshedo.tv>
2023-11-27 14:59:50 -0600

    Fix up PIDs

--- Keychron/Keychron_V2_QMK_ANSI_Keyboard.js
+++ Keychron/Keychron_V2_QMK_ANSI_Keyboard.js
@@ -1,7 +1,7 @@
 export function Name() { return "Keychron V2 QMK Keyboard"; }
 export function Version() { return "1.1.7;"; }
 export function VendorId() { return 0x3434; }
-export function ProductId() { return 0x0321; }
+export function ProductId() { return [0x0320, 0x0321]; }
 export function Publisher() { return "WhirlwindFX"; }
 export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
 export function Size() { return [15, 5]; }
