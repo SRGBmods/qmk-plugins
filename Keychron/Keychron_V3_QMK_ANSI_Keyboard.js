3807f7a66984a3e988207da20c1b632e387b0d62
Naitoshedo <hozuki@naitoshedo.tv>
2023-12-20 20:15:35 -0600

    Update Keychron_V3_QMK_ANSI_Keyboard.js
    Fix PID array to actually include ANSI keyboards.

--- Keychron/Keychron_V3_QMK_ANSI_Keyboard.js
+++ Keychron/Keychron_V3_QMK_ANSI_Keyboard.js
@@ -1,7 +1,7 @@
 export function Name() { return "Keychron V3 QMK ANSI Keyboard"; }
 export function Version() { return "1.1.7;"; }
 export function VendorId() { return 0x3434; }
-export function ProductId() { return [0x0331, 0x0332]; }
+export function ProductId() { return [0x0330, 0x0331]; }
 export function Publisher() { return "WhirlwindFX"; }
 export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
 export function Size() { return [18, 6]; }
