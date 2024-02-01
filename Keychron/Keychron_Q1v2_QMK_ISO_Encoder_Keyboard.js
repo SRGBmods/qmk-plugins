e7bbf909d6e27144aace6b522635a72ee04b05e9
Naitoshedo <hozuki@naitoshedo.tv>
2024-02-01 16:33:55 -0600

    Name cleanup

--- Keychron/Keychron_Q1v2_QMK_ISO_Encoder_Keyboard.js
+++ Keychron/Keychron_Q1v2_QMK_ISO_Encoder_Keyboard.js
@@ -1,4 +1,4 @@
-export function Name() { return "Keychron Q1 QMK Keyboard"; }
+export function Name() { return "Keychron Q1v2 ISO QMK Keyboard"; }
 export function Version() { return "1.1.7;"; }
 export function VendorId() { return 0x3434; }
 export function ProductId() { return 0x0109; }
