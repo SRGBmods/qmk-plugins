2756432b1a815a0c3e0d9e1a0bc1c17bfbc72185
Naitoshedo <hozuki@naitoshedo.tv>
2024-02-04 15:20:13 -0600

    Fix PID issues

--- Keychron/Keychron_Q1v2_QMK_ISO_Encoder_Keyboard.js
+++ Keychron/Keychron_Q1v2_QMK_ISO_Encoder_Keyboard.js
@@ -1,7 +1,7 @@
 export function Name() { return "Keychron Q1v2 ISO QMK Keyboard"; }
 export function Version() { return "1.1.7;"; }
 export function VendorId() { return 0x3434; }
-export function ProductId() { return 0x0109; }
+export function ProductId() { return [0x0108 ,0x0109]; }
 export function Publisher() { return "Genjimon"; }
 export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
 export function Size() { return [21, 6]; }
