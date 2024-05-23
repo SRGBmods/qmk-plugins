59d2022a0df7fa1163ba0269dc89a199bc600c41
Naitoshedo <hozuki@naitoshedo.tv>
2024-05-23 14:17:45 -0500

    Update Q3 Keyboards
    Thanks to leonardofelin for the corrections on the ANSI Encoder model

--- Keychron/Keychron_Q3_QMK_ANSI_Encoder_Keyboard.js
+++ Keychron/Keychron_Q3_QMK_ANSI_Encoder_Keyboard.js
@@ -1,7 +1,7 @@
 export function Name() { return "Keychron Q3 ANSI Encoder Keyboard"; }
 export function Version() { return "1.1.8"; }
 export function VendorId() { return 0x3434; }
-export function ProductId() { return [0x0121]; }
+export function ProductId() { return 0x0121; }
 export function Publisher() { return "WhirlwindFX"; }
 export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
 export function Size() { return [17, 6]; }
