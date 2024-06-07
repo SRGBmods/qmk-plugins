c436201230b8e510c9694276f310a40982cc7076
Naitoshedo <hozuki@naitoshedo.tv>
2024-06-07 07:00:05 -0500

    Update Akko 5075
    From x12013: updated the plugin to be more accurate as inspired by Keychron. had to bring out the ruler to measure led positions. also updated my note , found out the odd numbered side lights do work. not sure why in the original plugin they don't light up. still need the dummy1 and dummy2, else the leds wont line up with the keys.

--- Akko/Akko_5075_QMK_Keyboard.js
+++ Akko/Akko_5075_QMK_Keyboard.js
@@ -23,6 +23,7 @@ export function ControllableParameters() {
 }
 
 //Plugin Version: Built for Protocol V1.0.5
+//Note the dummy1,dummy2 are used for offsetting the LEDs, they do not light up. Effects will not work properly if removed.
 
 const vKeys = [
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
