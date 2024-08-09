7441b359801c7b477a0fb1f053df7c2ae0c08609
Naitoshedo <hozuki@naitoshedo.tv>
2024-08-09 00:13:36 -0500

    Update GMMK_TKL_QMK_ISO_Keyboard.js
    Push devicetype

--- GMMK/GMMK_TKL_QMK_ISO_Keyboard.js
+++ GMMK/GMMK_TKL_QMK_ISO_Keyboard.js
@@ -3,7 +3,8 @@ export function Version() { return "1.1.7;"; }
 export function VendorId() { return 0x0c45; }
 export function ProductId() { return 0x652f; }
 export function Publisher() { return "skank & WhirlwindFX"; }
-export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
+export function Documentation() { return "qmk/srgbmods-qmk-firmware"; }
+export function DeviceType() { return "keyboard"; }
 export function Size() { return [17, 6]; }
 export function DefaultPosition(){return [10, 100]; }
 export function DefaultScale(){return 8.0;}
