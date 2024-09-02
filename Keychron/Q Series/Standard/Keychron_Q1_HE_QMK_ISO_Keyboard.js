3332c46a77642da2c336281d58acd9a22a8609cb
Antoine <20756073+antt0n@users.noreply.github.com>
2024-09-02 17:48:37 +0200

    fix: add image for Keychron_Q1_HE_QMK_ISO_Keyboard

--- Keychron/Q Series/Standard/Keychron_Q1_HE_QMK_ISO_Keyboard.js
+++ Keychron/Q Series/Standard/Keychron_Q1_HE_QMK_ISO_Keyboard.js
@@ -330,5 +330,5 @@ export function Validate(endpoint) {
 }
 
 export function Image() {
-	return "";
+	return "https://raw.githubusercontent.com/SRGBmods/qmk-plugins/main/_images/Keychron_Q1_HE_ISO.png";
 }
