61095f1dc32f3566a5ce8a29bfd1a80f771ffc83
Naitoshedo <hozuki@naitoshedo.tv>
2024-10-28 13:38:14 -0500

    Update Alpaca_WhiteFox_Eclipse_QMK_Keyboard.js
    Disable underglow for now

--- Alpaca/Alpaca_WhiteFox_Eclipse_QMK_Keyboard.js
+++ Alpaca/Alpaca_WhiteFox_Eclipse_QMK_Keyboard.js
@@ -32,7 +32,7 @@ const vKeys = [
     38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27,     26, 25, 24, //15
     23,     22, 21, 20, 19, 18, 17, 16, 15, 14, 13,     12, 11, 10, //14
     9, 8, 7,             6,              5,  4,  3,  0,  1,  2,  //10
-    82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71 //12
+    //82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71 //12
 ];
 
 const vKeyNames = [
@@ -41,7 +41,7 @@ const vKeyNames = [
     "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter", "End", "PgDwn", //15
     "Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift", "Up Arrow", "Fn", //14
     "Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Right Win", "Right Ctrl", "Left Arrow",  "Down Arrow", "Right Arrow", //10
-    "Underglow 1", "Underglow 2", "Underglow 3", "Underglow 4", "Underglow 5", "Underglow 6", "Underglow 7", "Underglow 8", "Underglow 9", "Underglow 10", "Underglow 11", "Underglow 12" //12
+    //"Underglow 1", "Underglow 2", "Underglow 3", "Underglow 4", "Underglow 5", "Underglow 6", "Underglow 7", "Underglow 8", "Underglow 9", "Underglow 10", "Underglow 11", "Underglow 12" //12
 ];
 
 const vKeyPositions = [
@@ -50,7 +50,7 @@ const vKeyPositions = [
     [0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2],          [13, 2], [14, 2], [15, 2], //15
     [0, 3], 		[2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3],          [13, 3], [14, 3], [15, 3], //14
     [0, 4], [1, 4], [2, 4],					        [6, 4],					        [10, 4], [11, 4], [12, 4], [13, 4], [14, 4], [15, 4],  //10
-    [0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5] //12
+    //[0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5] //12
 ];
 
 let LEDCount = 0;
@@ -313,7 +313,7 @@ function sendColors(overrideColor) {
 function StreamLightingData(StartLedIdx, RGBData) {
 	const packet = [0x00, 0x24, StartLedIdx, Math.floor(RGBData.length / 3)].concat(RGBData);
 	device.write(packet, 33);
-	device.pause(10);
+	//device.pause(20);
 }
 
 function hexToRgb(hex) {
