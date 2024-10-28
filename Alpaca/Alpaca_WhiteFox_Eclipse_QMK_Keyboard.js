afcc25446b605aeb4fb1a2987e00479023e980f2
Naitoshedo <hozuki@naitoshedo.tv>
2024-10-28 18:26:28 -0500

    Update Alpaca_WhiteFox_Eclipse_QMK_Keyboard.js

--- Alpaca/Alpaca_WhiteFox_Eclipse_QMK_Keyboard.js
+++ Alpaca/Alpaca_WhiteFox_Eclipse_QMK_Keyboard.js
@@ -6,7 +6,7 @@ export function Publisher() { return "Cipher + WhirlwindFX"; }
 export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
 export function DeviceType() { return "keyboard"; }
 export function ConflictingProcesses() { return ["VIA.exe"]; }
-export function Size() { return [16, 6]; }
+export function Size() { return [16, 5]; }
 export function DefaultPosition(){return [10, 100]; }
 export function DefaultScale(){return 8.0;}
 /* global
