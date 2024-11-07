04c9ffed73feafbf6a23d9b5a38601b75a8b2558
Naitoshedo <hozuki@naitoshedo.tv>
2024-11-07 10:59:04 -0600

    Monsgeek M3 Image added

--- Monsgeek/Monsgeek_M3_Keyboard_TKL.js
+++ Monsgeek/Monsgeek_M3_Keyboard_TKL.js
@@ -323,6 +323,6 @@ export function Validate(endpoint) {
 	return endpoint.interface === 1;
 }
 
-export function Image() {
-	return "";
+export function ImageUrl() {
+	return "https://raw.githubusercontent.com/SRGBmods/qmk-plugins/main/_images/Monsgeek_M3.png";
 }
