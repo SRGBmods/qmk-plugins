1370cdb7d722d9ed25ab1cd82f844eb2efd5494d
Naitoshedo <hozuki@naitoshedo.tv>
2024-12-05 13:08:29 -0600

    Update GMMK_3_QMK_Keyboard.js
    //p75 ansi thanks to OuroborosWurm for development and testing - Signed off 12-5-2024 - Naitoshedo

--- _Needs-Testing/GMMK_3_QMK_Keyboard.js
+++ _Needs-Testing/GMMK_3_QMK_Keyboard.js
@@ -106,7 +106,7 @@ const vKeyPositionsISOp65 = [
 ];
 //end p65 iso
 
-//p75 ansi thanks to OuroborosWurm for development and testing - Signed off
+//p75 ansi thanks to OuroborosWurm for development and testing - Signed off 12-5-2024 - Naitoshedo
 const vKeysANSIp75 = [
      0,      1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 100,            //14
     13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,             //15
