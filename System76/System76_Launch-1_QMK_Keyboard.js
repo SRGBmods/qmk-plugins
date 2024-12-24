b5c7a9b8c4b36876a1f22f6468df703986086270
Naitoshedo <hozuki@naitoshedo.tv>
2024-12-24 13:05:19 -0600

    Create System76_Launch-1_QMK_Keyboard.js

---
+++ System76/System76_Launch-1_QMK_Keyboard.js
@@ -0,0 +1,362 @@
+export function Name() { return "Launch Configurable Keyboard (launch_1)"; }
+export function Version() { return "1.1.9"; }
+export function VendorId() { return 0x3384; }
+export function ProductId() { return 0x0001; }
+export function Publisher() { return "WhirlwindFX"; }
+export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
+export function DeviceType() { return "keyboard"; }
+export function ConflictingProcesses() { return ["VIA.exe"]; }
+export function Size() { return [15, 6]; }
+export function DefaultPosition(){return [10, 100]; }
+export function DefaultScale(){return 8.0;}
+/* global
+shutdownMode:readonly
+shutdownColor:readonly
+LightingMode:readonly
+forcedColor:readonly
+*/
+export function ControllableParameters() {
+    return [
+        {"property":"shutdownMode", "group":"lighting", "label":"Shutdown Mode", "type":"combobox", "values":["SignalRGB", "Hardware"], "default":"SignalRGB"},
+        {"property":"shutdownColor", "group":"lighting", "label":"Shutdown Color", "min":"0", "max":"360", "type":"color", "default":"#000000"},
+        {"property":"LightingMode", "group":"lighting", "label":"Lighting Mode", "type":"combobox", "values":["Canvas", "Forced"], "default":"Canvas"},
+        {"property":"forcedColor", "group":"lighting", "label":"Forced Color", "min":"0", "max":"360", "type":"color", "default":"#009bde"},
+    ];
+}
+
+//Plugin Version: Built for Protocol V1.0.5
+
+// The following may be helpful:
+// https://github.com/system76/qmk_firmware/tree/master/keyboards/system76
+// https://github.com/qmk/qmk_firmware/tree/master/keyboards/system76
+
+// https://gist.github.com/vinorodrigues/75b376e937f9e875244dff77fef2f8d7 - Determining LED Index to Physical Positions for the QMK RGB Matrix - Image Pixels (part1)
+// https://gist.github.com/vinorodrigues/07fd735683856b2a06c7c52b9b3878cb - Determining LED Index to Physical Positions for the QMK RGB Matrix - Image Pixels (part2)
+
+// Key matrix to LED index
+/*    A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   */
+/* 0  69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, */
+/* 1  68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, */
+/* 2  39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, */
+/* 3  38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26,     25, */
+/* 4  12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,      0,     */
+/* 5  13, 14, 15, 16, 17,     18,      19, 20, 21,    22, 23, 24  */
+const vKeys =
+[
+    69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, //15
+    68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, //15
+    39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, //15
+    38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26,     25, //14
+    12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,      0,     //13
+    13, 14, 15, 16, 17,     18,     19, 20, 21,     22, 23, 24  //12
+];
+
+// SignalRGB has no idea how to map keystrokes of Left or Right Function keys these are keys that are usually mapped on the bottom row (heatmap effects will not work for these keys):
+// "Ctrl", "Alt", "Left Ctrl", "Left Win", "Space", "Right Alt", "Fn", "Right Win", "Right Ctrl", "Menu", "Apps", "ContextMenu",
+
+const vKeyNames =
+[
+    "Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Del",                        "Home",                                                             //15
+    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+",        "Backspace",                            "Page Up",                                                          //15
+    "Tab",  "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",                                       "Page Down",                                                        //15
+    "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'",      "Enter",                                "End",                                                              //14
+    "Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/",         "Right Shift",                          "Up Arrow",                                                         //13
+    "Left Ctrl", "Left Fn", "Left Win", "Left Alt", "Left Space", "Right Space", "Right Ctrl", "Right Alt", "Righ Fn",       "Left Arrow","Down Arrow","Right Arrow",                   //12
+];
+
+// Here be Dragons as this layout index is super "fun", vKeyPositions needs some love based on this madness:
+/* {209, 51}, {190, 51}, {171, 51}, {156, 51}, {140, 51}, {125, 51}, {110, 51}, {95, 51}, {80, 51}, {65, 51},
+/* {49, 51}, {34, 51}, {11, 51}, {8, 64}, {27, 64}, {42, 64}, {57, 64}, {80, 64}, {110, 64}, {133, 64},
+/* {148, 64}, {167, 64}, {194, 64}, {209, 64}, {224, 64}, {224, 38}, {197, 38}, {178, 38}, {163, 38}, {148, 38},
+/* {133, 38}, {118, 38}, {103, 38}, {87, 38}, {72, 38}, {57, 38}, {42, 38}, {27, 38}, {8, 38}, {4, 26},
+/* {23, 26}, {38, 26}, {53, 26}, {68, 26}, {84, 26}, {99, 26}, {114, 26}, {129, 26}, {144, 26}, {159, 26},
+/* {175, 26}, {190, 26}, {205, 26}, {224, 26}, {224, 13}, {201, 13}, {182, 13}, {167, 13}, {152, 13}, {137, 13},
+/* {121, 13}, {106, 13}, {91, 13}, {76, 13}, {61, 13}, {46, 13}, {30, 13}, {15, 13}, {0, 13}, {0, 0},
+/* {15, 0}, {30, 0}, {46, 0}, {61, 0}, {76, 0}, {91, 0}, {106, 0}, {121, 0}, {137, 0}, {152, 0},
+/* {167, 0}, {182, 0}, {201, 0}, {224, 0}
+*/
+const vKeyPositions =
+[
+    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0],  [14, 0],                                                  //15
+    [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],  [14, 1],                                                  //15
+    [0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],  [14, 2],                                                  //15
+    [0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3],           [14, 3],                                                  //14
+    [0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4],          [13, 4],                                                            //13
+    [0, 5], [1, 5], [2, 5], [3, 5], [4, 5],         [6, 5],         [8, 5], [9, 5], [10, 5],          [12, 5], [13, 5],  [14, 5]                                                   //12
+];
+
+let LEDCount = 0;
+let IsViaKeyboard = false;
+const MainlineQMKFirmware = 1;
+const VIAFirmware = 2;
+const PluginProtocolVersion = "1.0.5";
+
+export function LedNames() {
+    return vKeyNames;
+}
+
+export function LedPositions() {
+    return vKeyPositions;
+}
+
+export function vKeysArrayCount() {
+    device.log('vKeys ' + vKeys.length);
+    device.log('vKeyNames ' + vKeyNames.length);
+    device.log('vKeyPositions ' + vKeyPositions.length);
+}
+
+export function Initialize() {
+    requestFirmwareType();
+    requestQMKVersion();
+    requestSignalRGBProtocolVersion();
+    requestUniqueIdentifier();
+    requestTotalLeds();
+    effectEnable();
+
+}
+
+export function Render() {
+    sendColors();
+}
+
+export function Shutdown(SystemSuspending) {
+
+    if(SystemSuspending) {
+        sendColors("#000000"); // Go Dark on System Sleep/Shutdown
+    } else {
+        if (shutdownMode === "SignalRGB") {
+            sendColors(shutdownColor);
+        } else {
+            effectDisable();
+        }
+    }
+
+    //vKeysArrayCount(); // For debugging array counts
+
+}
+
+function commandHandler() {
+    const readCounts = [];
+
+    do {
+        const returnpacket = device.read([0x00], 32, 10);
+        processCommands(returnpacket);
+
+        readCounts.push(device.getLastReadSize());
+
+        // Extra Read to throw away empty packets from Via
+        // Via always sends a second packet with the same Command Id.
+        if(IsViaKeyboard) {
+            device.read([0x00], 32, 10);
+        }
+    }
+    while(device.getLastReadSize() > 0);
+
+}
+
+function processCommands(data) {
+    switch(data[1]) {
+    case 0x21:
+        returnQMKVersion(data);
+        break;
+    case 0x22:
+        returnSignalRGBProtocolVersion(data);
+        break;
+    case 0x23:
+        returnUniqueIdentifier(data);
+        break;
+    case 0x24:
+        sendColors();
+        break;
+    case 0x27:
+        returnTotalLeds(data);
+        break;
+    case 0x28:
+        returnFirmwareType(data);
+        break;
+    }
+}
+
+function requestQMKVersion() //Check the version of QMK Firmware that the keyboard is running
+{
+    device.write([0x00, 0x21], 32);
+    device.pause(30);
+    commandHandler();
+}
+
+function returnQMKVersion(data) {
+    const QMKVersionByte1 = data[2];
+    const QMKVersionByte2 = data[3];
+    const QMKVersionByte3 = data[4];
+    device.log("QMK Version: " + QMKVersionByte1 + "." + QMKVersionByte2 + "." + QMKVersionByte3);
+    device.log("QMK SRGB Plugin Version: "+ Version());
+    device.pause(30);
+}
+
+function requestSignalRGBProtocolVersion() //Grab the version of the SignalRGB Protocol the keyboard is running
+{
+    device.write([0x00, 0x22], 32);
+    device.pause(30);
+    commandHandler();
+}
+
+function returnSignalRGBProtocolVersion(data) {
+    const ProtocolVersionByte1 = data[2];
+    const ProtocolVersionByte2 = data[3];
+    const ProtocolVersionByte3 = data[4];
+
+    const SignalRGBProtocolVersion = ProtocolVersionByte1 + "." + ProtocolVersionByte2 + "." + ProtocolVersionByte3;
+    device.log(`SignalRGB Protocol Version: ${SignalRGBProtocolVersion}`);
+
+
+    if(PluginProtocolVersion !== SignalRGBProtocolVersion) {
+        device.notify("Unsupported Protocol Version", `This plugin is intended for SignalRGB Protocol version ${PluginProtocolVersion}. This device is version: ${SignalRGBProtocolVersion}`, 2, "Documentation");
+    }
+
+    device.pause(30);
+}
+
+function requestUniqueIdentifier() //Grab the unique identifier for this keyboard model
+{
+    if(device.write([0x00, 0x23], 32) === -1) {
+        device.notify("Unsupported Firmware", "This device is not running SignalRGB-compatible firmware. Click the Documentation button to learn more.", 3, "Documentation");
+    }
+
+    device.pause(30);
+    commandHandler();
+}
+
+
+function returnUniqueIdentifier(data) {
+    const UniqueIdentifierByte1 = data[2];
+    const UniqueIdentifierByte2 = data[3];
+    const UniqueIdentifierByte3 = data[4];
+
+    if(!(UniqueIdentifierByte1 === 0 && UniqueIdentifierByte2 === 0 && UniqueIdentifierByte3 === 0)) {
+        device.log("Unique Device Identifier: " + UniqueIdentifierByte1 + UniqueIdentifierByte2 + UniqueIdentifierByte3);
+    }
+
+    device.pause(30);
+}
+
+function requestTotalLeds() //Calculate total number of LEDs
+{
+    device.write([0x00, 0x27], 32);
+    device.pause(30);
+    commandHandler();
+}
+
+function returnTotalLeds(data) {
+    LEDCount = data[2];
+    device.log("Device Total LED Count: " + LEDCount);
+    device.pause(30);
+}
+
+function requestFirmwareType() {
+    device.write([0x00, 0x28], 32);
+    device.pause(30);
+    commandHandler();
+}
+
+function returnFirmwareType(data) {
+    const FirmwareTypeByte = data[2];
+
+    if(!(FirmwareTypeByte === MainlineQMKFirmware || FirmwareTypeByte === VIAFirmware)) {
+        device.notify("Unsupported Firmware", "Click the Documentation button to learn more.", 3, "Documentation");
+    }
+
+    if(FirmwareTypeByte === MainlineQMKFirmware) {
+        IsViaKeyboard = false;
+        device.log("Firmware Type: Mainline");
+    }
+
+    if(FirmwareTypeByte === VIAFirmware) {
+        IsViaKeyboard = true;
+        device.log("Firmware Type: VIA");
+    }
+
+    device.pause(30);
+}
+
+function effectEnable() //Enable the SignalRGB Effect Mode
+{
+    device.write([0x00, 0x25], 32);
+    device.pause(30);
+}
+
+function effectDisable() //Revert to Hardware Mode
+{
+    device.write([0x00, 0x26], 32);
+    device.pause(30);
+}
+
+function createSolidColorArray(color) {
+    const rgbdata = new Array(vKeys.length * 3).fill(0);
+
+    for(let iIdx = 0; iIdx < vKeys.length; iIdx++) {
+        const iLedIdx = vKeys[iIdx] * 3;
+        rgbdata[iLedIdx] = color[0];
+        rgbdata[iLedIdx+1] = color[1];
+        rgbdata[iLedIdx+2] = color[2];
+    }
+
+    return rgbdata;
+}
+
+function grabColors(overrideColor) {
+    if(overrideColor) {
+        return createSolidColorArray(hexToRgb(overrideColor));
+    } else if (LightingMode === "Forced") {
+        return createSolidColorArray(hexToRgb(forcedColor));
+    }
+
+    const rgbdata = new Array(vKeys.length * 3).fill(0);
+
+    for(let iIdx = 0; iIdx < vKeys.length; iIdx++) {
+        const iPxX = vKeyPositions[iIdx][0];
+        const iPxY = vKeyPositions[iIdx][1];
+        const color = device.color(iPxX, iPxY);
+
+        const iLedIdx = vKeys[iIdx] * 3;
+        rgbdata[iLedIdx] = color[0];
+        rgbdata[iLedIdx+1] = color[1];
+        rgbdata[iLedIdx+2] = color[2];
+    }
+
+    return rgbdata;
+}
+
+function sendColors(overrideColor) {
+    const rgbdata = grabColors(overrideColor);
+
+    const LedsPerPacket = 9;
+    let BytesSent = 0;
+    let BytesLeft = rgbdata.length;
+
+    while(BytesLeft > 0) {
+        const BytesToSend = Math.min(LedsPerPacket * 3, BytesLeft);
+        StreamLightingData(Math.floor(BytesSent / 3), rgbdata.splice(0, BytesToSend));
+
+        BytesLeft -= BytesToSend;
+        BytesSent += BytesToSend;
+    }
+}
+
+function StreamLightingData(StartLedIdx, RGBData) {
+    const packet = [0x00, 0x24, StartLedIdx, Math.floor(RGBData.length / 3)].concat(RGBData);
+    device.write(packet, 33);
+}
+
+function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    const colors = [];
+    colors[0] = parseInt(result[1], 16);
+    colors[1] = parseInt(result[2], 16);
+    colors[2] = parseInt(result[3], 16);
+
+    return colors;
+}
+
+export function Validate(endpoint) {
+    return endpoint.interface === 1;
+}
